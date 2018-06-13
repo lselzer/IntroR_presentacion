1e770f7939aa37e1b1a1c2349ed1fb6d8c0e72e2
Luciano Selzer <luciano.selzer@gmail.com>
2018-06-13 07:38:06 -0300

    Añadido build pdf

--- _build.sh
+++ _build.sh
@@ -3,7 +3,7 @@ set -e
 
 for file in *.Rmd
 do
-    Rscript -e "rmarkdown::render(\"$file\")"
+    Rscript -e "rmarkdown::render(\"$file\", output_format = "pdf_document")"
 done
 
 Rscript -e "rmarkdown::render_site()"
